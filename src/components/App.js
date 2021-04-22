ba2d70f28b848066b0f4d5c0160b4a6bfaa92a09
Kallum Tolkien <kallumtolkien@gmail.com>
2021-04-22 13:44:05 +0100

    file structure created

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
-import './App.css';
+import React from "react";
+import "../styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Weather App
-      </h1>
+      <h1>Weather App</h1>
     </div>
   );
 }
